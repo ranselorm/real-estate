777fa4b20d26fad804e3a6595baac83c3448e2f5
Selorm <ranselorm@outlook.com>
2023-09-05 14:47:10 +0100

    Add button to banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -11,18 +11,23 @@ const Banner = () => {
   return (
     <section className="h-full max-h-[640px] mb-8 xl:mb-24">
       <div className="flex flex-col-reverse lg:flex-row items-center">
-        <div className="lg:ml-8 xl:ml-[135px] flex flex-col items-center lg:items-end text-center lg:text-left justify-end flex-1 px-4 lg:px-0 max-w-[480px]">
+        <div className="lg:ml-8 xl:ml-[135px] flex flex-col items-center lg:items-start text-center lg:text-left justify-end flex-1 px-4 lg:px-0 max-w-[480px]">
           <h1 className="text-4xl lg:text-[58px] font-semibold leading-none mb-6">
             <span className="text-[#008B8B]">Rent</span> Your Dream House With
             Us.
           </h1>
-          <p className=" flex-1 flex justify-end items-end">
+          <p className=" flex-1 flex justify-end items-end text-[18px]">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores
             recusandae quibusdam iste tempore eveniet reprehenderit eos sint a
             deleniti molestiae numquam at ut quo saepe obcaecati, illum, nobis
             suscipit possimus!
           </p>
-          <Link></Link>
+          <Link
+            to=""
+            className="bg-[#008B8B] text-white px-4 py-2 hover:bg-[#1d5353] transition-all duration-300 rounded-lg mt-10"
+          >
+            Discover More
+          </Link>
         </div>
         {/* image */}
         <div className="lg:flex flex-1 justify-end items-end mb-10 lg:mb-0">
